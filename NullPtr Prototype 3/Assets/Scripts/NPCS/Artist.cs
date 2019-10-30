ba0e565becb0d0a3e3391464ea57ea526c51e246
Harry <profawesum@gmail.com>
2019-10-30 15:00:11 +1300

    Working on NPC Dialogue Scripting
    Got it so the base is down, but needs to be finished

---
+++ NullPtr Prototype 3/Assets/Scripts/NPCS/Artist.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Artist : MonoBehaviour
+{
+    [SerializeField] ArtistDialogue dialogue;
+
+
+    public GameObject talkButton;
+    public GameObject sprite;
+    public bool inRange;
+    private SpriteRenderer npcSprite;
+    private float bobOffset;
+    public bool playerSpriteIsActive = true;
+    public GameObject talkPanel;
+    bool hasClicked = false;
+   
+    public bool badChoice = false;
+
+    public int day = 1;
+
+    void Start()
+    {
+        //bobbing
+        bobOffset = Random.Range(0.0f, 1.0f);
+        NPCArtistStats.Hope = 100;
+    }
+
+    void Update()
+    {
+        //bobbing
+        float offset = Random.Range(0.0f, 1.0f);
+
+        sprite.transform.localPosition = new Vector3(0, Mathf.Sin((bobOffset + Time.time) * 3) * 0.05f, 0);
+
+        //checks to see if the npc has lost hope
+        if (NPCArtistStats.Hope <= 0)
+        {
+            playerSpriteIsActive = false;
+        }
+
+    }
+
+    //changes the text based on the day 
+    public void dayNumber() {
+        dialogue.dayDialogue(day);
+    }
+
+    public void talk() {
+
+        //plays audio
+        FindObjectOfType<AudioManager>().Play("Click");
+        dayNumber();
+        //enable the buttons and text
+        talkPanel.SetActive(true);
+    }
+
+    public void agree() {
+        Debug.Log("Agree");
+        if (badChoice)
+        {
+            NPCArtistStats.Hope -= 10;
+        }
+        else if (!badChoice) {
+            NPCArtistStats.Hope += 10;
+        }
+    }
+
+    public void disagree() {
+        Debug.Log("Disagree");
+
+        if (!badChoice)
+        {
+            NPCArtistStats.Hope -= 10;
+        }
+        else if (badChoice) {
+            NPCArtistStats.Hope += 10;
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            inRange = true;
+            if (playerSpriteIsActive) {
+                talkButton.SetActive(true);        
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            inRange = false;
+            talkButton.SetActive(false);
+            talkPanel.SetActive(false);
+        }
+    }
+
+
+}
---
+++ NullPtr Prototype 3/Assets/Scripts/NPCS/ArtistDialogue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArtistDialogue : MonoBehaviour
+{
+
+    private Text dialogue;
+
+    private TextScrambler scrambler;
+
+
+    public void Start()
+    {
+        scrambler = this.GetComponent<TextScrambler>();
+        //scrambler.StringBuffer = "IDK";
+        //scrambler.forceUpdate = true;
+    }
+
+    //TODO: Add in the dialogue
+    public void dayDialogue(int day)
+    {
+        switch (day)
+        {
+            //day 1
+            case 1:
+                scrambler.StringBuffer = "Hello there";
+                scrambler.forceUpdate = true;
+                break;
+            //day 2
+            case 2:
+                break;
+            //day 3
+            case 3:
+                break;
+            //day 4
+            case 4:
+                break;
+            //day 5
+            case 5:
+                break;
+            //day 6
+            case 6:
+    
+                break;
+            //day 7
+            case 7:
+ 
+                break;
+            default:
+   
+                break;
+        }
+    }
+}
