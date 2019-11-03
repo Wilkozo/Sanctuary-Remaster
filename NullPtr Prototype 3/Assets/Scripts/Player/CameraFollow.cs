94793efe485537e36bc0c2ed63b18e5b7016a622
Wilkozox <44214305+Wilkozox@users.noreply.github.com>
2019-11-04 00:17:38 +1300

    Added Camera to movement
    In new testing scene

---
+++ NullPtr Prototype 3/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 CameraOffset;
+
+    // Prevents Jittery camera Movement
+    void LateUpdate ()
+    {
+        Vector3 desiredPos = target.position + CameraOffset;
+        Vector3 SmoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = target.position + CameraOffset;
+    }
+
+}
