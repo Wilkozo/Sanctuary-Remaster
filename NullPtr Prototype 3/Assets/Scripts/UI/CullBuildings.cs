9fe349ebfb3878f6d2a2f35dc81db50d482e014d
Harry <profawesum@gmail.com>
2019-11-06 14:15:44 +1300

    Fixing some materials

--- NullPtr Prototype 3/Assets/Scripts/UI/CullBuildings.cs
+++ NullPtr Prototype 3/Assets/Scripts/UI/CullBuildings.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class CullBuildings : MonoBehaviour
 {
-    private Color r;
+
+    public GameObject[] buildings;
+
+    private void Start()
+    {
+        buildings = GameObject.FindGameObjectsWithTag("CullBuilding");
+        foreach (GameObject building in buildings) {
+            building.GetComponent<MeshRenderer>().material.SetOverrideTag("RenderType", "Opaque");
+                //color = new Color(1, 1, 1, 0.99f);
+        }     
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "CullBuilding") {
+            other.GetComponent<MeshRenderer>().material.SetOverrideTag("RenderType", "TransparentFade");
             other.gameObject.GetComponent<MeshRenderer>().material.color = new Color(1,1,1,0.5f);
-           //r.a = 0.0f;
         }
     }
     private void OnTriggerExit(Collider other)
     {
+        other.GetComponent<MeshRenderer>().material.SetOverrideTag("RenderType", "");
         other.gameObject.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, 1.0f);
-        //other.gameObject.GetComponent<MeshRenderer>().enabled = true;
     }
 
 
