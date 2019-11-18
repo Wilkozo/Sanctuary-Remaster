750188ae40e19d882ec7bde2fa594da767ec8f78
John Strange <John.strange.nz@gmail.com>
2019-11-18 15:25:05 +1300

    Quest Ui Fix

---
+++ NullPtr Prototype 3/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QuestSystem;
+public class PlayerCollision : MonoBehaviour
+{
+    QuestManager questManager;
+    private void Start()
+    {
+        questManager = FindObjectOfType<QuestManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        questManager.CollectionCheck(collision.gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        questManager.CollectionCheck(collision.gameObject);
+    }
+}
